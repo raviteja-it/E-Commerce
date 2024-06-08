eaf8bab8a8b161e6d48894a9526c61fd5cdf01ba
Raviteja <raviteja@examtrakker.com>
2024-06-08 13:56:17 +0530

    jwt token

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const token = (id)=>{
-    return jwt.sign({id}, process.env.SECRET_KEY);
+    return jwt.sign({id}, process.env.SECRET_KEY, {expiresIn : "3d"});
 }
 
 module.exports = {token};
